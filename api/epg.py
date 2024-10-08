ffa778a27df3f8ebf21d7068c830bba03f377bff
Akshay Kumar <88607836+baswa-akshay@users.noreply.github.com>
2024-10-08 19:08:05 +0530

    Update epg.py

--- api/epg.py
+++ api/epg.py
@@ -1,29 +1,25 @@
 import requests
 import gzip
 import io
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from datetime import datetime
 import pytz
 import xml.etree.ElementTree as ET
 
 app = FastAPI()
 
-# Function to download and extract the XML file
 def download_and_extract_epg(url):
     response = requests.get(url)
     response.raise_for_status()  # Raise an error for bad responses
-    # Read the gzipped content directly into memory
     with gzip.GzipFile(fileobj=io.BytesIO(response.content)) as gz:
         xml_contents = gz.read()
     return xml_contents
 
-# Function to convert time to IST
 def convert_to_ist(time_str):
     dt = datetime.strptime(time_str, "%Y%m%d%H%M%S %z")
     ist = dt.astimezone(pytz.timezone('Asia/Kolkata'))
     return ist.strftime('%Y-%m-%d %H:%M:%S')
 
-# Function to get the current and upcoming program for a specific channel
 def get_current_and_upcoming_epg(xml_data, channel_id):
     xml_root = ET.fromstring(xml_data)
     current_time = datetime.now(pytz.timezone('Asia/Kolkata'))
@@ -33,14 +29,21 @@ def get_current_and_upcoming_epg(xml_data, channel_id):
     channel_info = None
 
     # Get channel information
-    for channel in xml_root.find("channel"):
-        if channel.attrib['id'] == channel_id:
-            channel_info = {
-                'id': channel.attrib['id'],
-                'name': channel.find("display-name").text,
-                'icon': channel.find("icon").attrib['src']
-            }
-            break
+    for channel in xml_root.findall("channel"):  # Make sure to use `findall`
+        try:
+            if channel.attrib['id'] == channel_id:
+                channel_info = {
+                    'id': channel.attrib['id'],
+                    'name': channel.find("display-name").text,
+                    'icon': channel.find("icon").attrib['src']
+                }
+                break
+        except KeyError as e:
+            print(f"KeyError: {e} for channel: {ET.tostring(channel)}")
+            continue
+
+    if channel_info is None:
+        raise HTTPException(status_code=404, detail=f"Channel ID '{channel_id}' not found in EPG.")
 
     programs = []
     # Get program information
@@ -83,7 +86,6 @@ def get_current_and_upcoming_epg(xml_data, channel_id):
         'Upcoming': next_program
     }
 
-# Main API endpoint
 @app.get("/api/epg")
 async def get_epg(id: int):
     channel_id = f'ts{id}'
@@ -96,4 +98,3 @@ async def get_epg(id: int):
     epg_data = get_current_and_upcoming_epg(xml_data, channel_id)
 
     return epg_data
-    
